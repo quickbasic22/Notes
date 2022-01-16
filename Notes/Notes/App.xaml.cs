74ccaffab136dfe8a0ff130aa0cc28274e65e494
quick <quick@COMPUTER3>
2022-01-16 14:36:28 -0500

    Add project files.

---
+++ Notes/Notes/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Notes.Data;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class App: Application
+    {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+    }
+}
